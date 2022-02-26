f4a5a260358dfe9e8992d952e6857321ca515129
MeenakshiPoorani <smeenakshipoorani@gmail.com>
2022-02-26 20:01:01 +0530

    Add MyModel file

---
+++ LearningGH/MyModel.swift
@@ -0,0 +1,14 @@
+//
+//  MyModel.swift
+//  LearningGH
+//
+//  Created by Meenakshi Poorani Seenivelan on 26/02/22.
+//
+
+import Foundation
+
+struct MyModel {
+    
+    var name:String?
+    
+}
