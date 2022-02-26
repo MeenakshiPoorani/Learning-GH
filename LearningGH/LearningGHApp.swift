6280e05401cbcfc9d5b6e489c35ff2d5cf13c4cf
MeenakshiPoorani <smeenakshipoorani@gmail.com>
2022-02-26 18:08:59 +0530

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Meenakshi Poorani Seenivelan on 26/02/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
